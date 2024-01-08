c341fc4958a78f18df7a162b27217c198be11701
carlitoselmago <ccarbonell@gmail.com>
2024-01-08 23:49:16 +0100

    Update exampleEnttecPro.py

--- exampleEnttecPro.py
+++ exampleEnttecPro.py
@@ -3,14 +3,14 @@ dmx = pyDMXController(port='/dev/ttyUSB0', device_type='enttec')
 
 #fade speed test
 
-#dmx.update_channel(6, 255)  # Set channel 6 to maximum
+dmx.update_channel(1, 255)  # Set channel 6 to maximum
 
-interval=0.00001
+interval=0.0001
 
-for i in range(8):
-    channel=i+1
-    print(channel)
-    dmx.update_channel(channel, 255)
+for i in range(255):
+    dmx.update_channel(2, i)
+    dmx.update_channel(3, i)
+    dmx.update_channel(4, i)
     dmx.run(interval)  
 
 dmx.close()
